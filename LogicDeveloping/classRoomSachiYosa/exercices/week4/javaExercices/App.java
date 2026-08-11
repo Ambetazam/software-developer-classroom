7acf88b1a65c71acffe88d54ab2671b10cf16724
ambet <ambetazam@gmail.com>
2026-08-11 13:13:33 -0400

    Exercices in Java

--- LogicDeveloping/classRoomSachiYosa/exercices/week4/javaExercices/App.java
+++ LogicDeveloping/classRoomSachiYosa/exercices/week4/javaExercices/App.java
@@ -2,7 +2,37 @@ public class App{
 
 	public static void main(String[] args){
 	
-		System.out.println("Hello World");
+		System.out.println("Hello World this Jean Steven Coding!!!");
+		//Summatory of two numbers exercice
+		//Define variables and type of variables
+
+		int number1, number2, sum;
+		
+		number1 = 1;
+		number2 = 4;
+
+		sum = number1 + number2;
+
+		//Concatenation Exercice
+		System.out.println("The summatory of two numbers is: " + sum);
+		
+		//some line jumps examples
+		//println,print,\n
+
+		System.out.println("============");
+		
+		String name, lastName;
+		int age;
+
+		name = "Robert";
+		lastName = "May";
+		age = 30;
+
+		System.out.println("El senor " + name + " de apellido " + lastName + " tiene una edad de: " + age + " primaveras!!!");
+
+
+
+
 	}
 
 
---
+++ LogicDeveloping/classRoomSachiYosa/exercices/week4/javaExercices/PerfilUsuario.java
@@ -0,0 +1,15 @@
+public class PerfilUsuario{
+	public static void main(String[] args){
+
+	String name, rol, accessLevel;
+
+	name = "Jean Steven";
+	rol = "Student";
+	accessLevel = "Administrator";
+
+	System.out.println("The Name is: " + name);
+	System.out.println("The Rol is: " + rol);
+	System.out.println("The Access Level is: " + accessLevel)
+
+	}
+}
