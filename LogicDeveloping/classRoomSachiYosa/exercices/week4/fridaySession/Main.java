9432c0d6ceb44ee488f79cf7ad762cc3e9b8d797
ambet <ambetazam@gmail.com>
2026-08-14 11:18:15 -0400

    Update Main

---
+++ LogicDeveloping/classRoomSachiYosa/exercices/week4/fridaySession/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+
+
+	public static void main(string[] args){
+
+
+		int AGE = 18;
+		int edadUsuario;
+		Scanner read = new Scanner(System.in);
+		System.out.println("How old are you:");
+		userAge = read.nextInt();
+
+
+		if (userAge >= AGE){
+			system.out.println("How old are you: ");
+
+		} else {
+
+			system.out.println("Your not enought older: ");
+
+		}
+
+		read.close();
+
+	}
+}
