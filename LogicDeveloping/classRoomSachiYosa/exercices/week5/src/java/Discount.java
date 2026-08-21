581cf11003df7083932051613e9b2594a3e65a37
ambet <ambetazam@gmail.com>
2026-08-21 12:33:16 -0400

    Update discount exercice

--- LogicDeveloping/classRoomSachiYosa/exercices/week5/src/java/Discount.java
+++ LogicDeveloping/classRoomSachiYosa/exercices/week5/src/java/Discount.java
@@ -7,17 +7,19 @@ public class Discount{
 
 		Scanner read = new Scanner(System.in);
 
-		int total;
+		double total;
 		double discount = 0.10;
-		int totalPayment = ((total * discount) - (total));
+		//double totalPayment = ((total * discount) - (total));
 
 		System.out.println("What is the total amount for you today?:");
 		total = read.nextInt();
 
-		if (total >= 100000){
+		if (total > 100000){
 			System.out.println("You apply for a 10% discount for this purchase!!!");
 			System.out.println("You save in this purchase: " + (total * discount) + " usd "+ " Thank you!");
-			System.out.println("You need to pay: " + totalPayment);
+			double totalPayment = total * discount;
+			double payment = (discount - totalPayment);
+			System.out.println("You need to pay: " + payment);
 		} else {
 			System.out.println("Thank you for you purchase!!");
 		}
