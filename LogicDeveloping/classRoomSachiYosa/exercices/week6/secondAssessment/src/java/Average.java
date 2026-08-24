8a620277955a377ddff2a64ba01c1aa8b3d0fa32
Ambetazam <ambetazam@gmail.com>
2026-08-24 23:46:45 +0200

    Update Average.java: Modified the scanner name to reader

--- LogicDeveloping/classRoomSachiYosa/exercices/week6/secondAssessment/src/java/Average.java
+++ LogicDeveloping/classRoomSachiYosa/exercices/week6/secondAssessment/src/java/Average.java
@@ -8,7 +8,7 @@ public class Average{
 
 		System.out.print("Cuanta cantidad de productos desea ingresar al lote del Catalogo?:");
 		int cantidadProductos = reader.nextInt();
-		scanner.nextLine();
+		reader.nextLine();
 
 		double costoTotal = 0.0;
 		int cantidadCategoriaAlta = 0; // Defino el nombre del acomulador para el ciclo for iniciando desde cero; 
@@ -18,10 +18,10 @@ public class Average{
 
 			// Ingreso de datos de usuario
 			System.out.println("Ingrese el nombre del producto " + productoActual + ":");
-			String nombreProducto = scanner.nextLine();
-			System.out.println("Ingrese el costo base de " + procductoActual + ":");
-			double costoBase = scanner.nextDouble();
-			scanner.nextln();
+			String nombreProducto = reader.nextLine();
+			System.out.println("Ingrese el costo base de " + productoActual + ":");
+			double costoBase = reader.nextDouble();
+			reader.nextln();
 
 			costoTotal += costoBase;
 
