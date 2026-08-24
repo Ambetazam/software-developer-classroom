60faf8b8ba28ee0835c2a7072735d8f79951251d
Ambetazam <ambetazam@gmail.com>
2026-08-24 23:38:47 +0200

    Update Average.java: added average and sum lines.

--- LogicDeveloping/classRoomSachiYosa/exercices/week6/secondAssessment/src/java/Average.java
+++ LogicDeveloping/classRoomSachiYosa/exercices/week6/secondAssessment/src/java/Average.java
@@ -12,9 +12,11 @@ public class Average{
 
 		double costoTotal = 0.0;
 		int cantidadCategoriaAlta = 0; // Defino el nombre del acomulador para el ciclo for iniciando desde cero; 
-		
+
+		// Inicio Ciclo For
 		for (int productoActual = 1; productoActual <= cantidadProductos; productoActual++){
 
+			// Ingreso de datos de usuario
 			System.out.println("Ingrese el nombre del producto " + productoActual + ":");
 			String nombreProducto = scanner.nextLine();
 			System.out.println("Ingrese el costo base de " + procductoActual + ":");
@@ -23,13 +25,30 @@ public class Average{
 
 			costoTotal += costoBase;
 
+			// Next I ask for the conditionals to categorize the prices according with the costo base ingresado por el usuario
 
+			if (costoBase < 10000){
+
+				System.out.println("El producto: " + nombreProducto + "es de Categoria Baja");
+
+			} else if {
+				System.out.println("El producto: " + nombreProducto + "es de Categoria Media");
+			} else {
+				System.out.println("El producto: " + nombreProducto + "es de Categoria Alta");
+				cantidadCategoriaAlta++; // Acomulador para los productos en Categoria Alta.
+			}
 		}
+		// Fin de Ciclo For
 
+		
 
+		// Next I continue with the average calculation.
 
-
-
-
+		double costoPromedio = costoTotal / cantidadProductos;
+		System.out.println("\n RESULTADO DE INGRESO DE LOTE AL CATALOGO \n");
+		System.out.println("El Costo Promedio del Lote Ingresado es de: " + costoPromedio + "$ COP");
+		System.out.println("La Cantidad de Productos Ingresados para Categoria Alta es: " + cantidadCategoriaAlta);
+		
+		reader.close();
 	}
 }
