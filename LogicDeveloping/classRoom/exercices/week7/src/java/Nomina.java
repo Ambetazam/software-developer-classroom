bdca93cff7a6e20ce526f4ac5300138b9e3921ac
ambet <ambetazam@gmail.com>
2026-09-01 12:47:49 -0400

    Added funtion to Nomina.java

--- LogicDeveloping/classRoom/exercices/week7/src/java/Nomina.java
+++ LogicDeveloping/classRoom/exercices/week7/src/java/Nomina.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 
 public class Nomina{
+
+public static Double SalarioBruto(double valorHora, double horasTrabajadas){
+
+	double salario = valorHora * horasTrabajadas;
+	return salario;
+}
+
+
+
 	public static void main(String[] args){
 
 
 	}
+
+
+
+
+
 
 }
