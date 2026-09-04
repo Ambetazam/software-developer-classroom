4c8b6870b79a20c3869c2c971dfb4b57304c6319
ambet <ambetazam@gmail.com>
2026-09-04 10:36:02 -0400

    Practice: Create login.java to practice functions

---
+++ level1/LogicDeveloping/classRoom/exercices/week7/src/java/Login.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+
+public class Login {
+
+
+
+	public static voud main(String[] args) {
+
+
+		Scanner read = new Scanner(System.in);
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
