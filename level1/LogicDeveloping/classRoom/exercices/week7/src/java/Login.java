e23ef714061d084bd100869615fd55669c2adb50
ambet <ambetazam@gmail.com>
2026-09-04 10:56:37 -0400

    Test: testing Login.java

--- level1/LogicDeveloping/classRoom/exercices/week7/src/java/Login.java
+++ level1/LogicDeveloping/classRoom/exercices/week7/src/java/Login.java
@@ -5,42 +5,52 @@ public class Login {
 
 
 
-	public static voud main(String[] args) {
+	public static void main(String[] args) {
 
 
 		Scanner read = new Scanner(System.in);
+
+		String user, paswd;
+		System.out.println("User");
+		user = read.next();
+
+
+		System.out.println("Password");
+		pswd = read.next();
+		verifyUser(user, pswd);
+		read.close();
 
 
 
 	}
 
+	//Functions
+
+	public static Boolean verifyUser(String user, String pswd){
+
+		String realUser = "Administrator";
+		String realPassword = "Changeme";
+
+		if (user = realUser == user && realPassword == pswd){
+
+			System.out.println("Hi");
+			return false;
+
+		}
+
+
+		else{
+
+			System.out.println("Incorrect");
+
+
+		}
 
 
 
+		return
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
 
 
 
